19909463a33d4b3376dd655876c6b59c997a23c5
theinternetbae <mr.farchan@gmail.com>
2017-09-20 19:04:15 +0700

    menambah form pada register form, menambah v-model pada tiap input, menambah @click pada button untuk menghubungkan method, menambah method register

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
     <h1>Register</h1>
+    <input type="email" name="email" placeholder="email" v-model="email"><br><br>
+    <input type="password" name="password" placeholder="password" v-model="password"><br>
+    <button @click="register">Register</button>
   </div>
 </template>
 <script>
 export default {
   data () {
     return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    register () {
+      console.log('register button was clicked', this.email, this.password)
     }
   }
 }
